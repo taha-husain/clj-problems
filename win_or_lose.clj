287c0dd8298e4e70be7f018c1c423c8824f44fd0
Taha Husain <taha@bigbinary.com>
2019-04-29 22:25:01 +0530

    refactoring

--- win_or_lose.clj
+++ win_or_lose.clj
@@ -2,56 +2,64 @@
  (:require [clojure.string :refer [split join]]))
 
 (def results (atom []))
+(def win? true?)
+(def win true)
+(def lose false)
 
-(defn main
+(defn remove-one
+ [x coll]
+ (let [[n m] (split-with (partial not= x) coll)]
+  (concat n (rest m))))
+
+(defn check-test-cases-constraint!
+ [iterations]
+ (when (or (> 1 iterations) (> iterations 10))
+  (println "LOSE")))
+
+(defn check-contraints!
+  [villains players no-of-players]
+  (when (or (not (every? #(and (<= 1 %) (<= % 100000)) villains))
+            (not (every? #(and (<= 1 %) (<= % 100000)) players))
+            (or (> 1 no-of-players) (> no-of-players 1000))
+            (not= (count villains) no-of-players)
+            (not= (count players) no-of-players))
+   (println "LOSE")))
+
+(defn player-vs-villain
+ [players villains]
+ (map
+   (fn [villain]
+    (let [player (->> players
+                      (filter (fn [player] (> player villain)))
+                      first)]
+     (if player
+      (do
+       (remove-one player players)
+       (remove-one villain villains)
+       win)
+      lose)))
+   villains))
+
+
+(defn play
  []
- (let [iterations (read-line)
-       iterations (Integer/parseInt iterations)]
-
-  (when (or (> 1 iterations) (> iterations 10))
-    (System/exit 0))
+ (let [iterations (Integer/parseInt (read-line))]
+  (check-test-cases-constraint! iterations)
 
   (dotimes [n iterations]
    (let [iteration (inc n)
-         no-of-players (read-line)
+         no-of-players (Integer/parseInt (read-line))
          villains (read-line)
          players (read-line)
+         villains (sort (map #(Integer/parseInt %) (split villains #" ")))
+         players (sort (map #(Integer/parseInt %) (split players #" ")))]
 
-         no-of-players (Integer/parseInt no-of-players)
-         villains (split villains #" ")
-         players (split players #" ")]
-
-
-     (when (or (or (> 1 no-of-players) (> no-of-players 1000))
-               (not= (count villains) no-of-players)
-               (not= (count players) no-of-players))
-       (System/exit 0))
+     (check-contraints! villains players no-of-players)
 
      (swap! results conj
-       (if (every?
-             true?
-             (map
-              (fn [villain]
-               (let [opponent (->> players
-                                   (filter
-                                     (fn [player]
-                                       (let [player (Integer/parseInt player)
-                                             villain (Integer/parseInt villain)]
-                                         (when (or (or (> 1 player) (> player 100000))
-                                                   (or (> 1 villain) (> villain 100000)))
-                                           (System/exit 0))
-                                         (> player villain))))
-                                   sort
-                                   first)]
-                (if opponent
-                 (do
-                  (remove #(= opponent %) players)
-                  (remove #(= villain %) villains)
-                  true)
-                 false)))
-              villains))
+       (if (every? win? (player-vs-villain players villains))
         "WIN"
         "LOSE"))))
   (println (join "\n" @results))))
 
-(main)
+(play)
